d708b88ca3bd26a8caed7d60568b83ad89c792f8
Christian-Ford <fjord.christian@gmail.com>
2019-11-12 12:37:10 -0600

    cta wireframed

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import React from 'react';
 import './App.css';
 
 import NavBar from './components/NavBar.js';
+import CallToAction from './components/CallToAction.js';
 
 function App() {
   return (
     <div className="App">
       <NavBar />
+      <CallToAction />
     </div>
   );
 }
---
+++ src/components/CallToAction.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+import '../css/callToAction.css';
+
+const CallToAction = () => {
+    return (
+        <div className="ctaWrapper">
+            <div className="ctaContent">
+                <div className="ctaLeftBox">
+                    <h1> Moob </h1>
+                </div>
+
+                <div className="ctaRightBox">
+                    <h1> Boom </h1>
+                </div>
+
+            </div>
+            
+        </div>
+    )
+}
+
+export default CallToAction;
