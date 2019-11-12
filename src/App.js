23ba3bf7c3501a9dcbad9712b23d8ff976594649
Christian-Ford <fjord.christian@gmail.com>
2019-11-12 13:21:36 -0600

    CTA Styling updated | Selected Work started

--- src/App.js
+++ src/App.js
@@ -4,12 +4,14 @@ import './App.css';
 
 import NavBar from './components/NavBar.js';
 import CallToAction from './components/CallToAction.js';
+import SelectedWork from './components/SelectedWork.js';
 
 function App() {
   return (
     <div className="App">
       <NavBar />
       <CallToAction />
+      <SelectedWork />
     </div>
   );
 }
---
+++ src/components/SelectedWork.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+import '../css/selectedWork.css';
+
+const SelectedWork = () => {
+    return (
+        <div className="selWork">
+            <div className="selContent">
+                <div className="selProjectOne">
+
+                </div>
+
+                <div className="selProjectTwo">
+
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default SelectedWork;
