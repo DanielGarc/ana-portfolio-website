107d570434377595ab04de14c6f822a501150ab9
Christian-Ford <fjord.christian@gmail.com>
2019-11-14 11:24:46 -0600

    Fixed bug issue

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,14 +1,10 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, NavLink } from "react-router-dom";
+
 
 import '../css/navbar.css';
 
-import Home from '../App';
-import About from '../About.js';
-
 const NavBar = () => {
     return (
-        <Router>
         <div className="navWrapper">
             <div className ="contentWrapper">
                 <div className="navLogo">
@@ -16,20 +12,12 @@ const NavBar = () => {
                 </div>
 
                 <div className="navLinks">
-                    <a> 
-                        <NavLink exact="/about"> About </NavLink>
-                    </a>
+                    <a> About </a>
                     <a> Projects </a>
                     <a> Contact </a>
                 </div>
             </div>
-
-            <div>
-                <Route exact path="/" component={Home} />
-                <Route exact path="/about" component={About} />
-            </div>
         </div>
-        </Router>
     )
 }
 
