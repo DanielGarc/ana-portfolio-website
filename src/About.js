dbf097bbe07692ef7b2263e9b456c37fb78be539
Christian-Ford <fjord.christian@gmail.com>
2019-11-14 10:06:33 -0600

    Added routing

--- src/About.js
+++ src/About.js
@@ -1,4 +1,6 @@
-import React from 'react'
+import React from 'react';
+
+import NavBar from './components/NavBar.js';
 
 const About = () => {
     return (
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, NavLink } from "react-router-dom";
 
 import '../css/navbar.css';
 
+import Home from '../App';
+import About from '../About.js';
+
 const NavBar = () => {
     return (
+        <Router>
         <div className="navWrapper">
             <div className ="contentWrapper">
                 <div className="navLogo">
@@ -11,12 +16,20 @@ const NavBar = () => {
                 </div>
 
                 <div className="navLinks">
-                    <a> About </a>
+                    <a> 
+                        <NavLink exact="/about"> About </NavLink>
+                    </a>
                     <a> Projects </a>
                     <a> Contact </a>
                 </div>
             </div>
+
+            <div>
+                <Route exact path="/" component={Home} />
+                <Route exact path="/about" component={About} />
+            </div>
         </div>
+        </Router>
     )
 }
 
