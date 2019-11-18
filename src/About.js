3337ffa50dbf1d10500595546772a1b4ff63f509
Christian-Ford <fjord.christian@gmail.com>
2019-11-18 10:54:00 -0600

    About page mock up done

--- src/About.js
+++ src/About.js
@@ -1,10 +1,12 @@
 import React from 'react';
 
 import NavBar from './components/NavBar.js';
+import AboutCTA from './components/aboutPage/AboutCTA.js';
 
 const About = () => {
     return (
         <div>
+            <AboutCTA />
             
         </div>
     )
