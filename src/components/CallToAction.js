3a1e348df38f9892401a428a26c5cc1e05dd8528
Ana Rosa <anarosa.lermas@gmail.com>
2019-11-21 23:53:40 -0600

    added package-lock to ignore list wip call to action added style and avatars to user section

--- src/components/CallToAction.js
+++ src/components/CallToAction.js
@@ -1,40 +1,37 @@
-import React from 'react';
+import React from "react";
 
-import '../css/callToAction.css';
+import "../css/callToAction.css";
 
-import headshot from '../assets/me.png';
+import headshot from "../assets/me.png";
 
 const CallToAction = () => {
-    return (
-        <div className="ctaWrapper">
-            <div className="ctaContent">
-                <div className="ctaLeftBox">
-                    <h1> Hola, I'm Ana Rosa. </h1>
-                    <p className="ctaText">
-                        I am a UX Designer, Technologist, and outdoors lover trying to create
-                        a user-friendly environment.
-                        <br />
-                        <br />
+  return (
+    <div className="ctaWrapper">
+      <div className="ctaContent">
+        <div className="ctaLeftBox">
+          <h1> Hola, I'm Ana Rosa. </h1>
+          <p className="ctaText">
+            I am a UX Designer, Technologist, and outdoors lover trying to
+            create a user-friendly environment.
+            <br />
+            <br />
+            I care deeply about people, and I love analyzing human behaviour.
+            <br />
+            <br />
+            Helping solve problems in a simple way is kind of my thing.
+          </p>
 
-                        I care deeply about people, and I love analyzing human behaviour.
-                        <br />
-                        <br />
-
-                        Helping solve problems in a simple way is kind of my thing.
-
-                    </p>
-
-                    <a className="ctaButton"> Check Out My Work! </a>
-                </div>
-
-                <div className="ctaRightBox">
-                    <img src={headshot} />
-                </div>
-
-            </div>
-            
+          <a className="ctaButton">
+            Check out my work!
+          </a>
         </div>
-    )
-}
+
+        <div className="ctaRightBox">
+          <img src={headshot} />
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default CallToAction;
