3ecc6576ce0d6524b1b3e97a742d3dd986dcbaf7
Daniel Garcia <danielgarc22@gmail.com>
2019-12-03 21:21:52 -0600

    Added deploy target

--- src/components/CaseStudyLink.js
+++ src/components/CaseStudyLink.js
@@ -2,48 +2,54 @@ import React from 'react';
 import { NavLink } from 'react-router-dom';
 
 import '../css/caseStudyLink.css';
+// cslImg,
+//   alignLeft = false,
+//   color,
+//   body,
+//   header,
+//   url
+class CaseStudyLink extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      hover: false
+    };
+  }
 
-const CaseStudyLink = ({
-  cslImg,
-  alignLeft = false,
-  color,
-  body,
-  header,
-  url
-}) => {
-  return (
-    <div
-      className={`cslWrapper ${
-        alignLeft ? 'cslWrapperLeft' : 'cslWrapperRight'
-      }`}
-    >
-      <img alt='Case Study' src={cslImg} />
+  render() {
+    var { cslImg, alignLeft = false, color, body, header, url } = this.props;
+    return (
       <div
-        style={{ backgroundColor: color }}
-        className={`cslTextWrapper ${
-          alignLeft ? 'cslTextWrapperLeft' : 'cslTextWrapperRight'
+        onMouseEnter={() => this.setState({ hover: true })}
+        onMouseLeave={() => this.setState({ hover: false })}
+        className={`cslWrapper ${
+          alignLeft ? 'cslWrapperLeft' : 'cslWrapperRight'
         }`}
       >
-        <p className='cslTextHeader'>{header}</p>
-        <div className='cslTextBody'>{body}</div>
-        {/* <NavLink className='cslTextLink' to='/fitstudy'> */}
-        <a
-          alt='Case Study'
-          href={url}
-          rel='noopener noreferrer'
-          target='_blank'
+        <img alt='Case Study' src={cslImg} />
+        <div
+          style={{ backgroundColor: color }}
+          className={`cslTextWrapper ${
+            alignLeft ? 'cslTextWrapperLeft' : 'cslTextWrapperRight'
+          }
+         `}
         >
-          <div className='cslTextLink'>
-            READ CASE STUDY
-            {/* <span className='cslTextArrow'>
-            <img alt='Right Arrow' src={arrowRight}></img>
-          </span> */}
-          </div>
-        </a>
-        {/* </NavLink> */}
+          <p className='cslTextHeader'>{header}</p>
+          <div className='cslTextBody'>{body}</div>
+          {/* <NavLink className='cslTextLink' to='/fitstudy'> */}
+          <a
+            alt='Case Study'
+            href={url}
+            rel='noopener noreferrer'
+            target='_blank'
+          >
+            <div className='cslTextLink'>READ CASE STUDY</div>
+          </a>
+          {/* </NavLink> */}
+        </div>
       </div>
-    </div>
-  );
-};
+    );
+  }
+}
 
 export default CaseStudyLink;
