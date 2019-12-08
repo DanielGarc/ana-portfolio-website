38f24a878848030a951cbebf4b8d13c0f87ea215
Daniel Garcia <danielgarc22@gmail.com>
2019-12-08 00:26:13 -0600

    about page done

--- src/components/aboutPage/AboutCTA.js
+++ src/components/aboutPage/AboutCTA.js
@@ -3,10 +3,10 @@ import React from 'react';
 import '../../css/aboutPage.css';
 
 import AboutImage from '../../assets/Image.png';
-// import Hiker from '../../assets/hiker.png';
-// import Instructor from '../../assets/instructor.png';
-// import Dog from '../../assets/dog.png';
-// import Runner from '../../assets/runner.png';
+import Hiker from '../../assets/hiker.png';
+import Instructor from '../../assets/instructor.png';
+import Dog from '../../assets/dog.png';
+import Runner from '../../assets/runner.png';
 
 const AboutCTA = () => {
   return (
@@ -39,29 +39,35 @@ const AboutCTA = () => {
             </div>
 
             <div className='characteristics'>
-              <AboutPill height={41} width={104}>
-                CARING
-              </AboutPill>
-              <AboutPill height={41} width={119}>
-                FRIENDLY
-              </AboutPill>
-              <AboutPill height={41} width={119}>
-                CREATIVE
-              </AboutPill>
-              <AboutPill height={41} width={148}>
-                PASSIONATE
-              </AboutPill>
-              <AboutPill height={41} width={140}>
-                OPTIMISTIC
-              </AboutPill>
-              <AboutPill height={41} width={164}>
-                HARD WORKER
-              </AboutPill>
+              <div className='aboutPillRow'>
+                <AboutPill height={41} width={104}>
+                  CARING
+                </AboutPill>
+                <AboutPill height={41} width={119}>
+                  FRIENDLY
+                </AboutPill>
+                <AboutPill height={41} width={119}>
+                  CREATIVE
+                </AboutPill>
+              </div>
+              <div className='aboutPillRow'>
+                <AboutPill height={41} width={148}>
+                  PASSIONATE
+                </AboutPill>
+                <AboutPill height={41} width={140}>
+                  OPTIMISTIC
+                </AboutPill>
+              </div>
+              <div className='aboutPillRow'>
+                <AboutPill height={41} width={164}>
+                  HARD WORKER
+                </AboutPill>
+              </div>
             </div>
           </div>
 
           <div className='aboutText'>
-            <div className='aboutMe'>ABOUT</div>
+            <div className='aboutHeader'>ABOUT</div>
             <div className='paragraphText'>
               I've always been worried about people and finding ways to be
               useful, but that's how I am, I never thought that one day I would
@@ -89,62 +95,69 @@ const AboutCTA = () => {
               and design, I found HCI and with that the UX professional career.
               I bought books, watched videos and did courses on HCI and UX
               design and found it; my passion.
-              <br />
             </div>
 
             <div className='goalSection'>
-              <h2 style={{ textAlign: 'left', marginLeft: '6%' }}> Goals </h2>
+              <div className='aboutHeader'>GOALS</div>
               <div className='goals'>
-                <ul>
-                  <li> Deliver A Good Experience </li>
-                  <li> Help People Reach Their Maximum Potential</li>
-                  <li> Always Have A Welcoming Experience</li>
-                </ul>
+                <div className='goalCircleBullet' />
+                <div className='goalText'>Deliver good experience</div>
               </div>
-              <h2 style={{ textAlign: 'left', marginLeft: '6%' }}>
-                {' '}
-                Frustrations{' '}
-              </h2>
               <div className='goals'>
-                <ul>
-                  <li> Not Being Able To Communicate Correctly With People</li>
-                  <li> Human Behaviour </li>
-                </ul>
+                <div className='goalCircleBullet' />
+                <div className='goalText'>
+                  Help people reach their maximum potential
+                </div>
+              </div>
+              <div className='goals'>
+                <div className='goalCircleBullet' />
+                <div className='goalText'>Always have a welcoming presence</div>
+              </div>
+            </div>
+            <div className='goalSection'>
+              <div className='aboutHeader'>FRUSTATIONS</div>
+              <div className='goals'>
+                <div className='goalCircleBullet' />
+                <div className='goalText'>
+                  Not being able to communicate correctly with people
+                </div>
+              </div>
+              <div className='goals'>
+                <div className='goalCircleBullet' />
+                <div className='goalText'>Human behavior</div>
               </div>
             </div>
           </div>
         </div>
-        {/* <div className='otherSection'>
-          <div className='otherWrapper'>
-            <h2>Who Am I Other Than A UX Designer?</h2>
+      </div>
+      <div className='otherSection'>
+        <div className='otherWrapper'>
+          <div className='otherSectionHeader'>
+            Who am I besides a UX designer?
+          </div>
 
-            <div className='hobbies'>
-              <div className='myHobby'>
-                <img alt='' src={Hiker} />
-                <p> Hiker </p>
-              </div>
+          <div className='hobbies'>
+            <div className='myHobby'>
+              <img alt='' src={Hiker} />
+              <p> Hiker </p>
+            </div>
 
-              <div className='myHobby'>
-                <img alt='' src={Instructor} />
-                <p> Fitness Instructor </p>
-              </div>
+            <div className='myHobby'>
+              <img alt='' src={Instructor} />
+              <p> Fitness Instructor </p>
+            </div>
 
-              <div className='myHobby'>
-                <img alt='' src={Dog} />
-                <p> Ghost's Mom </p>
-              </div>
+            <div className='myHobby'>
+              <img alt='' src={Dog} />
+              <p> Ghost's Mom </p>
+            </div>
 
-              <div className='myHobby'>
-                <img alt='' src={Runner} />
-                <p> Runner </p>
-              </div>
+            <div className='myHobby'>
+              <img alt='' src={Runner} />
+              <p> Runner </p>
             </div>
           </div>
-        </div> */}
-        {/* <div className='aboutContact'>
-          <h2> Contact :</h2>
-          <p> anarosa.lermas@gmail.com</p>
-        </div> */}
+        </div>
       </div>
     </div>
   );
