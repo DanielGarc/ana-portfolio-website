e02a136dc76101ebe7002c040f72b7b698700a54
Daniel Garcia <danielgarc22@gmail.com>
2020-02-13 21:56:00 -0600

    fit case study progress

--- src/components/caseStudies/FitnessStudy.js
+++ src/components/caseStudies/FitnessStudy.js
@@ -3,10 +3,12 @@ import ProjectOverview from './shared/ProjectOveriew';
 import '../../css/fitStudy.css';
 
 import FitStudy from '../../assets/banner.png';
+import avatarTop from '../../assets/top_avatar.png';
+import avatarBottom from '../../assets/bottom_avatar.png';
 
 const FitnessStudy = () => {
   return (
-    <React.Fragment>
+    <div>
       <div className='fitStudyWrapper'>
         <div className='fitStudyContent'>
           <div>
@@ -22,7 +24,43 @@ const FitnessStudy = () => {
         </div>
       </div>
       <ProjectOverview />
-    </React.Fragment>
+      <div></div>
+      <div className='user-section-wrapper'>
+        <div className='userSection'>
+          <div className='trainers'>
+            <img alt='' className='avatarStyle' src={avatarTop} />
+            <div className='trainerBox'>
+              <h2> Trainers </h2>
+              <p>
+                “It’s hard to keep up with all the trainees progress.”
+                <br />
+                “If someone hasn’t pay I don’t like to remind them personally.”
+                <br />
+                “I have more than one certification and people don’t know them.”
+                <br />
+              </p>
+            </div>
+          </div>
+
+          <div className='trainers'>
+            <img alt='' className='avatarStyle' src={avatarBottom} />
+
+            <div className='trainerBox'>
+              <h2> Trainees </h2>
+              <p>
+                “The apps that they have used are not engaging.”
+                <br />
+                “It’s hard for me to finished the programs I start.”
+                <br />
+                “I don’t want to pay for something that I don’t know if it’s
+                going to work.”
+                <br />
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   );
 };
 
--- src/components/caseStudies/shared/ProjectOveriew.js
+++ src/components/caseStudies/shared/ProjectOveriew.js
@@ -3,53 +3,146 @@ import React from 'react';
 import Team from '../../../assets/team.svg';
 import Time from '../../../assets/time.svg';
 import Tools from '../../../assets/tools.svg';
+import ArrowRight from '../../../assets/arrow forward.svg';
+import Arrow from '../../../assets/arrow up.svg';
 
 import '../../../css/projectoverview.css';
 
 const ProjectOverview = () => {
   return (
-    <div className='po-wrapper'>
-      <div className='po-container'>
-        <div className='po-item-container'>
-          <div className='po-item'>
-            <img alt='' src={Team} />
-            <p>
-              1 UX designer &
-              <br />5 Software developers
-            </p>
+    <div>
+      <div className='po-wrapper'>
+        <div className='po-container'>
+          <div className='po-item-container'>
+            <div className='po-item'>
+              <img alt='' src={Team} />
+              <p>
+                1 UX designer &
+                <br />5 Software developers
+              </p>
+            </div>
+            <div className='po-item'>
+              <img alt='' src={Time} />
+              <p>5 Weeks</p>
+            </div>
+            <div className='po-item'>
+              <img alt='' src={Tools} />
+              <p>
+                Sketch, Whimsical,
+                <br />
+                InVision, Paper and pencil
+              </p>
+            </div>
           </div>
-          <div className='po-item'>
-            <img alt='' src={Time} />
-            <p>5 Weeks</p>
-          </div>
-          <div className='po-item'>
-            <img alt='' src={Tools} />
-            <p>
-              Sketch, Whimsical,
+          <div className='po-overview-text'>
+            <div className='po-overview-text-header'>Overview</div>
+            <div className='po-overview-text-block'>
+              Fitness instructors have a lot to work on, not just classes but,
+              working with people, with their goals, not only changing their
+              body but their minds.
               <br />
-              InVision, Paper and pencil
-            </p>
+              <br />
+              This is hard to do when you have to write everything on paper or
+              don’t have enough time to work with them personally, or remind
+              them all about their classes or their goals.
+              <br />
+              <br />
+              Trainers are looking for something that it can be with them all
+              the time, always up to date with what’s going on with their
+              clients, and removing that layer of awkwardness to go and ask for
+              the payments.
+            </div>
+          </div>
+          <div className='po-overview-card-container'>
+            <div className='po-overview-card-left-content'>
+              <div className='po-overview-card-content-header'>Challenge</div>
+              <div className='po-overview-card-content-text'>
+                How can we build an engaging app that lets trainers and trainees
+                manage their workouts and payments.
+              </div>
+            </div>
+            <div className='po-overview-card-right-content'>
+              <div className='po-overview-card-content-header'>Solution</div>
+              <div className='po-overview-card-content-text'>
+                An app that will be up to date with what’s going on with their
+                clients and send reminders to trainees about payments.
+              </div>
+            </div>
           </div>
         </div>
-        <div className='po-overview-text'>
-          <div className='po-overview-text-header'>Overview</div>
-          <div className='po-overview-text-block'>
-            Fitness instructors have a lot to work on, not just classes but,
-            working with people, with their goals, not only changing their body
-            but their minds.
-            <br />
-            <br />
-            This is hard to do when you have to write everything on paper or
-            don’t have enough time to work with them personally, or remind them
-            all about their classes or their goals.
-            <br />
-            <br />
-            Trainers are looking for something that it can be with them all the
-            time, always up to date with what’s going on with their clients, and
-            removing that layer of awkwardness to go and ask for the payments.
+      </div>
+      <div className='po-dp-wrapper'>
+        <div className='po-dp-container'>
+          <div className='po-dp-caption'>Design Process</div>
+          <div className='po-dp-arrow-top'>
+            <img alt='' src={Arrow} />
           </div>
-          <br />
-          <br />
+          <div className='po-dp-diagram-container'>
+            <div className='po-dp-diagram-item-container'>
+              <div
+                className='po-dp-diagram-header'
+                style={{ color: '#A23222' }}
+              >
+                DISCOVERY
+              </div>
+              <div className='po-dp-diagram-text'>
+                Research user
+                <br />
+                problems and
+                <br />
+                goals
+              </div>
+            </div>
+            <div className='po-dp-diagram-item-container arrow'>
+              <img alt='' src={ArrowRight} />
+            </div>
+            <div className='po-dp-diagram-item-container'>
+              <div
+                className='po-dp-diagram-header'
+                style={{ color: '#A16026' }}
+              >
+                DEFINE
+              </div>
+              <div className='po-dp-diagram-text'>
+                Narrow and
+                <br />
+                measure the
+                <br />
+                problems
+              </div>
+            </div>
+            <div className='po-dp-diagram-item-container arrow'>
+              <img alt='' src={ArrowRight} />
+            </div>
+            <div className='po-dp-diagram-item-container'>
+              <div
+                className='po-dp-diagram-header'
+                style={{ color: '#064268' }}
+              >
+                IDEATE
+              </div>
+              <div className='po-dp-diagram-text'>
+                Brainstorming
+                <br />
+                solutions
+              </div>
+            </div>
+            <div className='po-dp-diagram-item-container arrow'>
+              <img alt='' src={ArrowRight} />
+            </div>
+            <div className='po-dp-diagram-item-container'>
+              <div
+                className='po-dp-diagram-header'
+                style={{ color: '#0A723D' }}
+              >
+                PROTOTYPE
+              </div>
+              <div className='po-dp-diagram-text'>Test</div>
+            </div>
+          </div>
+          <div className='po-dp-arrow-bot'>
+            <img alt='' src={Arrow} />
+          </div>
         </div>
       </div>
     </div>
