77c9808a0360fe6a328cbfb90d1a27aaefe27124
Daniel Garcia <danielgarc22@gmail.com>
2020-03-23 23:57:32 -0500

    fixed contact form spacing with footer

--- src/components/shared/ContactForm.js
+++ src/components/shared/ContactForm.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import '../../css/contactForm.css';
+import Section from './Section';
 
 export default class ContactForm extends React.Component {
   constructor(props) {
@@ -13,58 +14,63 @@ export default class ContactForm extends React.Component {
   render() {
     const { status } = this.state;
     return (
-      <div className='cFormWrapper'>
-        <div className='cFormContent'>
-          <div className='cFormLeftContent'>
-            <h2>DON'T BE A STRANGER</h2>
-            <h1>
-              Let's
-              <br />
-              connect!
-            </h1>
-          </div>
-          <div className='cFormRightContent'>
-            <h2>DON'T BE A STRANGER</h2>
-            <form action='https://formspree.io/mzbzpgjk' method='POST'>
-              <input
-                type='hidden'
-                name='_next'
-                value='http://localhost:3000/'
-              />
-              <label className='cFormRightContentNameLabel'>Name:</label>
-              <input
-                required
-                type='text'
-                id='fname'
-                name='firstname'
-                placeholder='Enter your name'
-              ></input>
-              <label style={{ marginTop: 15 }}>Email:</label>
-              <input
-                required
-                type='email'
-                name='email'
-                placeholder='Enter your email address'
-              />
-              <label style={{ marginTop: 15 }}>Message:</label>
-              <textarea
-                minLength='1'
-                rows='2'
-                type='text'
-                name='message'
-                placeholder='Words go here'
-              />
+      <>
+        <div className='cFormWrapper'>
+          <div className='cFormContent'>
+            <div className='cFormLeftContent'>
+              <h2>DON'T BE A STRANGER</h2>
+              <h1>
+                Let's
+                <br />
+                connect!
+              </h1>
+            </div>
+            <div className='cFormRightContent'>
+              <h2>DON'T BE A STRANGER</h2>
+              <form action='https://formspree.io/mzbzpgjk' method='POST'>
+                <input
+                  type='hidden'
+                  name='_next'
+                  value='http://localhost:3000/'
+                />
+                <label className='cFormRightContentNameLabel'>Name:</label>
+                <input
+                  required
+                  type='text'
+                  id='fname'
+                  name='firstname'
+                  placeholder='Enter your name'
+                ></input>
+                <label style={{ marginTop: 15 }}>Email:</label>
+                <input
+                  required
+                  type='email'
+                  name='email'
+                  placeholder='Enter your email address'
+                />
+                <label style={{ marginTop: 15 }}>Message:</label>
+                <textarea
+                  minLength='1'
+                  rows='2'
+                  type='text'
+                  name='message'
+                  placeholder='Words go here'
+                />
 
-              {status === 'SUCCESS' ? (
-                <p>Thanks!</p>
-              ) : (
-                <input type='submit' value='Submit mesage' />
-              )}
-              {status === 'ERROR' && <p>Ooops! There was an error.</p>}
-            </form>
+                {status === 'SUCCESS' ? (
+                  <p>Thanks!</p>
+                ) : (
+                  <input type='submit' value='Submit mesage' />
+                )}
+                {status === 'ERROR' && <p>Ooops! There was an error.</p>}
+              </form>
+            </div>
           </div>
         </div>
-      </div>
+        <Section wrapperColor='#fff' sectionColor='#fff'>
+          <div className='section-spacer'></div>
+        </Section>
+      </>
     );
   }
 
