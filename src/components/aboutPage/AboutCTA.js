9d75c1bd3cd2596d3ebff1422b65edf1e2250efb
Ana Rosa <anarosa.lermas@gmail.com>
2020-03-30 21:19:15 -0500

    changes on about

--- src/components/aboutPage/AboutCTA.js
+++ src/components/aboutPage/AboutCTA.js
@@ -92,26 +92,34 @@ const AboutCTA = () => {
             <div className='paragraphText'>
             I've always been worried about people and finding ways to be useful because that's who I am. 
             I've always dreamed about helping change the world but never thought I would actually have a chance of being a part of it.
-              <br />
-              <br />
-              After moving to the United States, I was working as a Systems' Technician, 
-              where I began to notice that I was far more interested in the user experience and that I was worried about how
- the users I work with would react to changes in the system and applications.
 
               <br />
               <br />
-              In the meanwhile, a couple of friends asked me to design the workflow and experience of their new application idea, and in doing so I found myself 
-              really immersed in those sketches and screens, sometimes spending all day on them without realizing it. I remember thinking at that time "This is how people should feel about 
-              their work: excited about what they are doing, feeling like somehow you're doing something to change the world for better".
+              AWhen I moved to the United States, I started working for a local company as a Systems’ Technician. 
+              I began to notice that I was far more interested in the user experience and that I was worried 
+              about how the users I work with would react to changes in the system and applications.
+
+
               <br />
               <br />
-              At this time I was quite new in the design world and I started researching which careers were involved in the working of prototypes, 
-              experiences, user behavior, and design. That's when I found HCI and with 
-              that my path to UX. I bought books, watched videos and did courses on HCI and UX design and finally found it: my passion.
+              In the meantime, a couple of friends asked me to design the workflow and experience of their new 
+              application idea. In doing so I found myself really immersed in those sketches and screens, 
+              sometimes spending all day on them without realizing it. I remember thinking at that time, 
+              "This is how people should feel about their work: excited about what they are doing, feeling like somehow you're doing something to change the world for better."
+
               <br />
               <br />
-              I am excited for what the future departs for me and for the next projects I will be able to work in. I consider myself an extraordinarily hard-working and passionate person 
-              and I am committed to continue getting experience in UX and do everything I can to contribute with my team to get the best result of whatever we are set to do.
+              At this time I was quite new in the design world and I started researching which careers were 
+              involved in the working of prototypes, experiences, user behavior, and design. That's when I 
+              found HCI and with that my path to UX. I bought books, watched videos, and did courses on HCI and UX design and finally found it: my passion.
+
+              <br />
+              <br />
+              I am excited for what the future holds for me and for the next projects I will be able to work on. 
+              I consider myself an extraordinarily hard-working and passionate person and I am committed to 
+              continue getting experience in UX. I will do everything I can to contribute with my team to get 
+              the best result of whatever we are set out to do.
+
             </div>
             <div className='section-spacer' />
             <div className='section-spacer' />
